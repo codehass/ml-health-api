4630ae32793c4bd3befa9afcfde06f4233c235d2
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-10-29 11:25:08 +0100

    create metric_model function

--- ml/pipeline.py
+++ ml/pipeline.py
@@ -1,11 +1,15 @@
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.feature_selection import SelectKBest, f_classif
 from sklearn.model_selection import train_test_split
+
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from pipeline import model_pipeline, split_data
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 
 def split_data(data, target, test_size=0.2, random_state=42):
@@ -36,3 +40,12 @@ def model_pipeline(num_list, model):
             ("model", model),
         ]
     )
+
+
+def metric_model(y_test, y_pred):
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+
+    return accuracy, precision, recall, f1
