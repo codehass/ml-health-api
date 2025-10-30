98c38bb678adf2e3413bb8a61d84e4444f202342
elhidarinouhayla <elhidarinouhayla@gmail.com>
2025-10-30 12:16:03 +0100

    configuration du database

--- database.py
+++ database.py
@@ -1,1 +1,17 @@
-# this file for SQL database connection
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+DATABASE_URL = "sqlite:///./data_sante.db"
+
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
